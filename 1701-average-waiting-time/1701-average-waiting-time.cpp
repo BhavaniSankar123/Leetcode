5e4fca63e9fe53f19af121b0e393decc673afc6b
BhavaniSankar123 <137146540+BhavaniSankar123@users.noreply.github.com>
2024-07-09 08:39:22 +0530

    Time: 129 ms (97.35%), Space: 76.5 MB (83.82%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
        long long n=customers.size(),wt=customers[0][1];
         customers[0][1]+=customers[0][0];
         for(int i=1;i<n;i++)
