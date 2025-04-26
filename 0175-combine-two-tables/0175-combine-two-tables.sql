2e6386061facf0ae8bd3ab806c9576304e767ad3
Kondaka Bhavani Sankar <137146540+BhavaniSankar123@users.noreply.github.com>
2025-04-26 13:16:00 +0530

    Time: 406 ms (81.61%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,1 +1,11 @@
-select per.firstname,per.lastname,adr.city,adr.state from person per left join address adr on per.personid = adr.personid
+SELECT 
+    per.firstname,
+    per.lastname,
+    adr.city,
+    adr.state 
+FROM 
+    person per
+LEFT JOIN
+    address adr 
+ON 
+    per.personid = adr.personid
