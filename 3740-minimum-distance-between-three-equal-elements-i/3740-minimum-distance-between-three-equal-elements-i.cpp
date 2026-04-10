4f464f02988b1c770a88f6c2a300def0a2d560df
Kondaka Bhavani Sankar <137146540+BhavaniSankar123@users.noreply.github.com>
2026-04-11 00:01:55 +0530

    Time: 0 ms (100%), Space: 30.8 MB (18.28%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -4,7 +4,7 @@ public:
         map<int,vector<int>> ump;
         if(nums.size()<3)
         return -1;
-        int ans = -1;
+        int ans = INT_MAX;
         for(int i=0;i<nums.size();i++)
            ump[nums[i]].push_back(i);
 
@@ -12,12 +12,10 @@ public:
         {
             if(ele.second.size()<3)
             continue;
-            if(ans == -1)
-            ans = INT_MAX;
             int n= ele.second.size();
             for(int i=0;i<n-2;i++)
                 ans = min(ans,2*(ele.second[i+2] - ele.second[i]));
         }
-        return ans;
+        return ans == INT_MAX ? -1 : ans;
     }
 };
