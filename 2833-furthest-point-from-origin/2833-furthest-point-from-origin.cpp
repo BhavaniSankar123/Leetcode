996bd5b232ce155f653500869117a17a3ea9a7f5
Kondaka Bhavani Sankar <137146540+BhavaniSankar123@users.noreply.github.com>
2026-04-25 15:31:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (90.91%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int l=0,_=0,i=0,n=moves.length();
+        int l=0,r=0,_=0,i=0,n=moves.length();
         while(i<n)
         {
             l+= (moves[i] == 'L');
+            r+= (moves[i] == 'R');
             _+= (moves[i] == '_');
             i++;
+            // cout<<l<<r<<_<<"\n";
         }
-        if(2 * l< n - _)
-        return n- l- l;
-        return 2 * (l + _) - n;
+        if(l<r)
+        return r + _ - l;
+        return l + _ - r;
     }
 };
