961531ce3257e0871099f39a075d28c51f1e849a
Kondaka Bhavani Sankar <137146540+BhavaniSankar123@users.noreply.github.com>
2026-04-26 11:24:47 +0530

    Time: 55 ms (100%), Space: 146.2 MB (3.94%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,3 +1,9 @@
+static const auto _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
