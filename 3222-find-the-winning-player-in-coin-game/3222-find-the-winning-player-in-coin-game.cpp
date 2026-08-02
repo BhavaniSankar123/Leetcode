8c3cebe44e493a83ceb9003aa2bf095b4a79bbf6
Kondaka Bhavani Sankar <137146540+BhavaniSankar123@users.noreply.github.com>
2026-08-02 20:06:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.25%) - LeetHub

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     string winningPlayer(int x, int y) {
-        int f=0;
-        while(x>=1 && y>=4)
-        {
-            x--;
-            y-=4;
-            f=!f;
-        }
-        return f==0 ?  "Bob" : "Alice";
+        int m = min(x,y/4);
+        return m&1 ?   "Alice" : "Bob" ;
     }
 };
